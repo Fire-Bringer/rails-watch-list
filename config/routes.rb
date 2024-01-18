be950618ee9f5b845626b32dd8ff801e00896519
Rashad DuPaty <radupaty@gmail.com>
2024-01-18 10:20:44 +0900

    added delete action to lists controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  # Sets rootpage as the lists index
   root to: 'lists#index'
 
-  resources :lists, only: %i[show new create] do
+  # Leave out the edit and update actions for the List Controller
+  resources :lists, except: %i[edit update] do
+    # Only these routes to create new bookmarks within lists
     resources :bookmarks, only: %i[new create]
+    resources :reviews, only: :create
   end
   resources :bookmarks, only: %i[destroy]
+  resources :reviews, only: %i[destroy]
 end
